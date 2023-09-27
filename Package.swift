64d5c19d9617af7c706ff4cfcbde6b0ca2491ffc
William.Weng <William.Weng@iit.com.hk>
2023-09-27 14:33:03 +0800

    - Swift 5.7 / iOS 14

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "WWKeychain",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "WWKeychain", targets: ["WWKeychain"]),
